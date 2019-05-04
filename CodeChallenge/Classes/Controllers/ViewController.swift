0fe7a6c1a07bcebca47c3dd658196ac8dff56cea
Sebastien Audeon <sebastien.peek@gmail.com>
2019-05-04 09:29:23 -0700

    Initial project setup

---
+++ CodeChallenge/Classes/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeChallenge
+//
+//  Created by Sebastien Audeon on 5/4/19.
+//  Copyright © 2019 LuckyDay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
